084586ca553c9c725d0217bbb1b8374fba542e29
Fabrício Antõnio <fabricio.ss2117@gmail.com>
2023-03-30 12:06:09 -0300

    DIO Bank concluído

--- index.ts
+++ index.ts
@@ -1,5 +1,12 @@
 // DIO Bank
 
+/* É possível ralizar:
+criação de conta;
+depósitos;
+saques;
+cancelamento de conta.
+ */
+
 interface IConta {
     nomeTitular: string
     saldo?: number
@@ -8,6 +15,7 @@ interface IConta {
 class Conta {
     private nomeTitular: string
     private saldo: number
+    private ativa: boolean = true
 
     constructor({
         nomeTitular,
@@ -17,10 +25,43 @@ class Conta {
         this.saldo = saldo
         console.log(`${this.nomeTitular} bem-vindo ao DIO Bank`)
     }
+
+    meuSaldo = (): number => this.saldo
+
+    depositar = (valor: number):  void | boolean => {
+        if(!this.ativa){
+            console.log('Conta desativada, impossível ralizar depósito!')
+            return this.ativa
+        }
+        this.saldo += valor
+        console.log(`você depositou ${valor}. Seu novo saldo é ${this.saldo}`)
+    }
+
+    sacar = (valor: number): boolean => {
+        if(!this.ativa){
+          console.log('Conta desativa, impossível realizar saque')
+          return this.ativa
+        }
+
+        if(valor <= this.saldo){
+            this.saldo -= valor
+            console.log(`Você sacou ${valor}. Seu novo saldo é ${this.saldo}`)
+            return this.ativa
+          }
+
+        console.log(`Seu saldo atual é insuficiente. Seu saldo atual é de ${this.saldo}`)
+        return this.ativa
+    }
+
+    cancelarConta = () => {
+        if (this.saldo === 0){
+        this.ativa = false
+        console.log('Conta desativada')
+        return this.ativa
+        }
+
+        console.log(`Impossível desativar conta, a conta de ${this.nomeTitular}. Conta com pendência!`)
+    }
 }
 
-const contaA: Conta = new Conta({nomeTitular: 'Fabríco'})
-const contaB: Conta = new Conta({nomeTitular: 'Pedro', saldo: 10})
-
-console.log(contaA)
-console.log(contaB)
+const conta: Conta = new Conta({nomeTitular: 'Fabrício'})
